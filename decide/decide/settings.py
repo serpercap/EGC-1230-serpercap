180c127bb9d4398414c0b902c1cf53e691b43d19
serpercap <serpercap@alum.us.es>
2021-02-12 10:22:06 +0100

    #1 (Bug) Arreglado el fallo en decide

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Especifica aqui tu UVUS
-UVUS = 'UVUS-to-Change'
+UVUS = 'serpercap'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-#from django.urls import path, include
+from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
 
 
